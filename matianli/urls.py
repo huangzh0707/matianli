5c9a8ff4dd3a0f2714389b2c6157015e3b739719
huangzh0707 <huangzihong2009@gmail.com>
2017-04-25 18:40:28 +0800

    django example 1

--- matianli/urls.py
+++ matianli/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib.auth.models import User
+from django.contrib import admin
 from rest_framework import routers, serializers, viewsets
 
 # Serializers define the API representation.
@@ -36,5 +37,7 @@ router.register(r'users', UserViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^search/', include('search.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
