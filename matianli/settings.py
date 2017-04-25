260cd62afa4d2bee835fd4383d805556ed94b719
huangzh0707 <huangzihong2009@gmail.com>
2017-04-25 18:21:00 +0800

    api restframework example

--- matianli/settings.py
+++ matianli/settings.py
@@ -72,6 +72,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'matianli.wsgi.application'
 
 
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 #
