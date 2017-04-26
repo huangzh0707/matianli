23e8dbc62556752002399b2506113ec53377afd8
huangzh0707 <huangzihong2009@gmail.com>
2017-04-26 16:44:47 +0800

    part 2 done

--- search/models.py
+++ search/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class TopList(models.Model):
+    top_project = models.CharField(max_length=200)
+
+class SearchResults(models.Model):
+    project_name = models.CharField(max_length=200)
+    project_brief = models.CharField(max_length=500)
+
