0af6bcb10e74d6af950c08a4a5264dafa8d0bacb
huangzh0707 <huangzihong2009@gmail.com>
2017-04-26 19:56:47 +0800

    trends2

--- trends/models.py
+++ trends/models.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class TopList(models.Model):
+    company = models.CharField(max_length= 500)
+    amount = models.PositiveIntegerField(default=0)
+    rounds = models.CharField(max_length= 200)
+    date = models.DateField('funding date')
+
+class Profile(models.Model):
+    name = models.ForeignKey(TopList, on_delete=models.CASCADE)
+    location = models.CharField(max_length=200)
