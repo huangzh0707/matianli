c130e51ed815b1d9c11704c81ab02b42ebf50476
huangzh0707 <huangzihong2009@gmail.com>
2017-04-27 16:33:15 +0800

    database connected

--- trends/views.py
+++ trends/views.py
@@ -2,8 +2,15 @@
 from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import TopList
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hell, this is trending page!")
+    companies = TopList.objects.order_by('amount')[:3]
+    template = loader.get_template('trends/index.html')
+    context = {
+        'companies': companies
+    }
+    return HttpResponse(template.render(context, request))
